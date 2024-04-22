b319961e70a36042844ac2cadfe49a4efe59ca0d
Richard Benny <richardvictorbenny@gmail.com>
2024-04-22 12:09:44 +0100

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CSY2091AS2"
+include(":app")
+ 
