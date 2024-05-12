6dbafd2ecf0b838d63e1c8ec73f1b2385c5df2fa
Richard Benny <richardvictorbenny@gmail.com>
2024-05-12 02:06:58 +0100

    added delete student layout

--- app/src/main/java/com/example/csy2091as2/Admin/AdminActivity.kt
+++ app/src/main/java/com/example/csy2091as2/Admin/AdminActivity.kt
@@ -1,8 +1,19 @@
 package com.example.csy2091as2.Admin
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.csy2091as2.CommentAdapter
 import com.example.csy2091as2.R
 import com.example.csy2091as2.RegisterActivity
 import com.example.csy2091as2.databinding.ActivityAdminBinding
@@ -19,5 +30,25 @@ class AdminActivity : AppCompatActivity() {
         binding.layAddStudent.setOnClickListener{
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+        binding.layDeleteStudent.setOnClickListener{
+            openDeleteDrawer()
+        }
+    }
+
+    private fun openDeleteDrawer() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.delete_drawer_layout)
+
+
+
+
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
     }
 }
